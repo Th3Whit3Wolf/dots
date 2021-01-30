b8c8c4296fc193de6422986cc47f04fdbf3ed52d
David Karrick <the.white.wolf.is.1337@gmail.com>
2021-01-30 09:49:28 +0000

    Remove barbar animations, leave telescope on esc

--- private_dot_config/private_nvim/private_lua/private_plugins/private_init.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/private_init.lua
@@ -12,6 +12,11 @@ g["db_ui_winwidth"] = 35
 -- Vsnip snippet directories
 g["vsnip_snippet_dir"] = G.vim_path .. "snippets"
 
+
+-- Barbar
+-- Enable/disable animations
+g["bufferline.animation"] = false
+
 -- Vista
 --- How each level is indented and what to prepend.
 --- This could make the display more compact or more spacious.
@@ -130,46 +135,48 @@ require"toggleterm".setup{
 }
 
 require('telescope').setup{
-    defaults = {
-      vimgrep_arguments = {
-        'rg',
-        '--color=never',
-        '--no-heading',
-        '--with-filename',
-        '--line-number',
-        '--column',
-        '--smart-case'
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = require('telescope.actions').close
       },
-      prompt_position = "bottom",
-      prompt_prefix = ">",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      sorting_strategy = "descending",
-      layout_strategy = "horizontal",
-      layout_defaults = {
-        -- TODO add builtin options.
-      },
-      file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-      file_ignore_patterns = {"target/*"},
-      generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-      shorten_path = true,
-      winblend = 0,
-      width = 0.75,
-      preview_cutoff = 120,
-      results_height = 1,
-      results_width = 0.8,
-      border = {},
-      borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-      color_devicons = true,
-      use_less = true,
-      set_env = { ['COLORTERM'] = 'truecolor' }, -- default { }, currently unsupported for shells like cmd.exe / powershell.exe
-      file_previewer = require'telescope.previewers'.cat.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_cat.new`
-      grep_previewer = require'telescope.previewers'.vimgrep.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_vimgrep.new`
-      qflist_previewer = require'telescope.previewers'.qflist.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_qflist.new`
-  
-      -- Developer configurations: Not meant for general override
-      buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
-    }
+    },
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case'
+    },
+    prompt_position = "bottom",
+    prompt_prefix = ">",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
+    layout_strategy = "horizontal",
+    layout_defaults = {
+      -- TODO add builtin options.
+    },
+    file_sorter =  require'telescope.sorters'.get_fzy_sorter,
+    file_ignore_patterns = {"target/*"},
+    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    shorten_path = true,
+    winblend = 0,
+    width = 0.75,
+    preview_cutoff = 120,
+    results_height = 1,
+    results_width = 0.8,
+    border = {},
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+    color_devicons = true,
+    use_less = true,
+    set_env = { ['COLORTERM'] = 'truecolor' }, -- default { }, currently unsupported for shells like cmd.exe / powershell.exe
+    file_previewer = require'telescope.previewers'.vim_buffer_cat.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_cat.new`
+    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_vimgrep.new`
+    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_qflist.new`
+  },
 }
 
 require('nvim-autopairs').setup()
